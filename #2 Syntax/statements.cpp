72491008e891d3d668bf02136ab03fd7ba51d959
mirsaOffice <mirsaoffice@gmail.com>
2025-04-30 00:00:28 +0700

    Create statements.cpp

---
+++ #2 Syntax/statements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Pernyataan C++
+    //Program komputer adalah daftar "intruksi" yang akan "dieksekusi" oleh komputer. Dalam bahasa pemrograman, intruksi pemrograman ini disebut statement (Pernyataan). Pernyataan berikut "memerintahkan" kompiler untuk mencetak teks "Hello World" ke layar :
+    cout << "Hello World!";
+    cout << "\n======================";
+
+    //Banyak Pernyataan
+    //Sebagian besar program C++ mengandung banyak pernyataan.
+    //Pernyataan-pernyataan tersebut dieksekusi satu per satu, sesuai urutan penulisannya:
+    cout << "\nHello World!";
+    cout << "\nHave a good day!";
+    cout << "\n======================";
+}
