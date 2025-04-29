248d6c16a566bb0dca9a8efd6e8f8a6acb94e35b
mirsaOffice <mirsaoffice@gmail.com>
2025-04-29 23:58:23 +0700

    Create home.cpp

---
+++ #1 Home/home.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Editor "Coba sendiri" kami memudahkan Anda mempelajari C++. Anda dapat mengedit kode C++ dan melihat hasilnya di peramban anda
+    cout << "Hello World!";
+    return 0;
+}
