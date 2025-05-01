ad390e8259135518d1371d2438e63fbe88410cd6
mirsaOffice <mirsaoffice@gmail.com>
2025-05-01 11:33:43 +0700

    Add files via upload

---
+++ #3 Outputs/printNumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Print Numbers C++
+    //Anda juga dapat menggunakannya cout() untuk mencetak angka. Namun, tidak seperti teks, kami tidak memasukkan angka di dalam tanda kutip ganda :
+    cout << 3;
+    cout << "\n======================\n";
+
+    //Anda juga dapat melakukan perhitungan matematika :
+    cout << 3 + 3;
+    cout << "\n" << 2 * 5;
+    return 0;
+}
---
+++ #3 Outputs/printText.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Keluaran C++(Cetak Teks)
+    //Objek cout, bersama dengan << operator, digunakan untuk mengeluarkan nilai dan mencetak teks. Ingatlah untuk menyertakan tanda kutip ganda ("") pada teks:
+    cout << "Hello World!";
+    cout << "\n======================";
+
+    //Anda dapat menambahkan objek sebanyak cout yang anda inginkan. Namun, perlu diingat bahwa ini tidak menyisipkan baris baru di akhir output:
+    cout << "\nHello World!";
+    cout << "\nI am learning C++";
+    cout << "\n======================";
+
+    
+}
