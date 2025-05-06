f9a29387ea70e3d5ed131002f8814bfee999b981
mirsaOffice <mirsaoffice@gmail.com>
2025-05-06 10:19:53 +0700

    Create constants.cpp

---
+++ #5 Variables/constants.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Konstanta
+    //Bila Anda tidak ingin orang lain (atau diri Anda sendiri) mengubah nilai variabel yang ada, gunakan const kata kunci (ini akan mendeklarasikan variabel sebagai "konstan", yang berarti tidak dapat diubah dan hanya dapat dibaca)
+    //Anda harus selalu mendeklarasikan variabel sebagai konstanta bila Anda memiliki nilai yang tidak mungkin berubah :
+    const int minutesPerHour = 60;
+    cout << minutesPerHour << endl;
+    cout << "======================" << endl;
+
+    //Catatan Tentang Konstanta
+    //Ketika Anda mendeklarasikan variabel konstan, variabel tersebut harus diberi nilai :
+    
+}
