1e205651414812d2944cf8a6c8af338d59d0b6cf
mirsaOffice <mirsaoffice@gmail.com>
2025-05-09 20:57:23 +0700

    Create basicDataTypes.cpp

---
+++ #7 Data Types/basicDataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Tipe Data C++
+    //Seperti yang dijelaskan dalam bab Variabel, variabel dalam C++ harus memiliki tipe data tertentu :
+    int myNum = 5;
+    float myFloatNum = 5.99;
+    double myDoubleNum = 9.98;
+    char myLetter = 'D';
+    bool myBoolean = true;
+    string myText = "Hello";
+
+    //boolean 1 byte
+    //char 1 byte
+    //int 2 or 4 bytes
+    //float 4 bytes
+    //double 8 bytes
+}
