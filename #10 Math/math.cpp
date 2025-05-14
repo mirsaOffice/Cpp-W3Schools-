4d847e470e1cc67b6ea7b649dcfdbec0146d7918
mirsaOffice <mirsaoffice@gmail.com>
2025-05-14 10:39:30 +0700

    Create math.cpp

---
+++ #10 Math/math.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    //Matematika C++
+    //C++ memiliki banyak fungsi yang memungkinkan Anda melakukan tugas matematika pada angka.
+
+    //Maks dan Min
+    //Fungsi ini dapat digunakan untuk menemukan nilai x dan y tertinggi: max(x,y):
+    cout << max(5,10) << endl;
+    cout << "======================" << endl;
+
+    //Dan fungsi ini dapat digunakan untuk menemukan nilai x dan y terendah: min(x,y):
+    cout << min(5,10) << endl;
+    cout << "======================" << endl;
+
+    //Pustaka C++ <cmath>
+    //fungsi lainnya, seperti sqrt(akar kuadrat), round(membulatkan angka) dan log(logaritma natural), dapat ditemukan di <cmath> file header:
+    cout << sqrt(64) << endl;
+    cout << round(2.6) << endl;
+    cout << log(2) << endl;
+    cout << "======================" << endl;
+}
