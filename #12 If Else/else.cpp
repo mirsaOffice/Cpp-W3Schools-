bc18205e5c5001abf3aac96e38c6f9bb0330fe90
mirsaOffice <mirsaoffice@gmail.com>
2025-05-21 11:40:03 +0700

    Create else.cpp

---
+++ #12 If Else/else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    //Else
+    //Gunakan else pernyataan untuk menentukan blok kode yang akan dieksekusi jika kondisinya adalah false.
+    int time = 20;
+    if(time < 18){
+        cout << "Good day." << endl;
+    } else {
+        cout << "Good evening." << endl;
+    }
+}
